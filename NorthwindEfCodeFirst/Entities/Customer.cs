1f216206410cf615be829276b76c5d44225506e2
Kadir Hali <kadir.hali@iksirsoft.com>
2023-07-14 10:30:22 +0300

    Add project files.

---
+++ NorthwindEfCodeFirst/Entities/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace NorthwindEfCodeFirst.Entities;
+
+public class Customer
+{
+    public Customer()
+    {
+        Orders = new List<Order>();  //Object null referance hatası almamak için 
+    }
+    public string CustomerID { get; set; }
+    public string ContactName { get; set; }
+    public string CompanyName { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    public List<Order> Orders { get; set; }
+}
---
+++ NorthwindEfCodeFirst/Entities/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace NorthwindEfCodeFirst.Entities;
+
+public class Order
+{
+    public int OrderID { get; set; }
+    public string CustomerID { get; set; }
+    public string ShipCity { get; set; }
+    public string ShipCountry { get; set; }
+    public DateTime OrderDate { get; set; }
+    public Customer Customer { get; set; }
+}
